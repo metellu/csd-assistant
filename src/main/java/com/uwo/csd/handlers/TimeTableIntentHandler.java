a4708d41b20fab27e25559c869d3440fce7aa800
Wang LYU <wlyu@WangdeMacBook-Pro.local>
2019-06-14 16:38:02 -0400

    modify the code to enable manually delegate

--- src/main/java/com/uwo/csd/handlers/TimeTableIntentHandler.java
+++ src/main/java/com/uwo/csd/handlers/TimeTableIntentHandler.java
@@ -2,6 +2,7 @@ package com.uwo.csd.handlers;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.impl.IntentRequestHandler;
+import com.amazon.ask.model.Intent;
 import com.amazon.ask.model.IntentRequest;
 import com.amazon.ask.model.Response;
 import com.amazon.ask.model.Slot;
@@ -19,38 +20,36 @@ public class TimeTableIntentHandler implements IntentRequestHandler {
     public boolean canHandle(HandlerInput input, IntentRequest intentRequest){
         return input.matches(intentName("TimeTableIntent"));
     }
-    public Optional<Response> handle(HandlerInput input, IntentRequest intentRequest){
+    public Optional<Response> handle(HandlerInput input, IntentRequest intentRequest) {
         String speechText = "Sorry, I failed to fetch the time.";
-        IntentRequest request = (IntentRequest)input.getRequestEnvelope().getRequest();
+        IntentRequest request = (IntentRequest) input.getRequestEnvelope().getRequest();
         Map<String, Slot> slots = request.getIntent().getSlots();
         String courseName = "";
         String courseCode = "";
-        if( slots!=null ){
-            if( slots.containsKey("course_name") ) {
+        if (slots != null) {
+            if (slots.containsKey("course_name")) {
                 Slot nameSlot = slots.get("course_name");
                 if (nameSlot != null) {
                     courseName = nameSlot.getValue();
-                    if(courseName!=null){
+                    if (courseName != null) {
                         speechText = "course code is null.";
                     }
                 }
             }
-            if( slots.containsKey("course_code")) {
+            if (slots.containsKey("course_code")) {
                 Slot codeSlot = slots.get("course_code");
-                if ( codeSlot != null ){
+                if (codeSlot != null) {
                     courseCode = codeSlot.getValue();
                     speechText = "slot is not null.";
 
-                    if(courseCode == null){
+                    if (courseCode == null) {
                         speechText += "course code is null.";
-                    }
-                    else if(courseCode.equals("")){
+                    } else if (courseCode.equals("")) {
                         speechText += "course code is empty.";
                     }
                 }
             }
-        }
-        else{
+        } else {
             speechText = "slot is null.";
         }
 
@@ -58,107 +57,54 @@ public class TimeTableIntentHandler implements IntentRequestHandler {
         String timeConcat = "";
         String error = "";
 
-        try {
-            AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
-            Map<String, AttributeValue> exprAttr = new HashMap<String, AttributeValue>();
-            if ( courseCode!=null && !courseCode.equals("") ) {
-                exprAttr.put(":code", new AttributeValue().withS(courseCode));
-                ScanRequest scanReq = new ScanRequest()
-                        .withTableName("t_csd_timetable")
-                        .withFilterExpression("contains(course_code,:code)")
-                        .withProjectionExpression("course_name,course_time")
-                        .withExpressionAttributeValues(exprAttr);
-                ScanResult result = client.scan(scanReq);
-                Map<String,String> courseInfo = retrieveCourseTime(result);
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+        Map<String, AttributeValue> exprAttr = new HashMap<String, AttributeValue>();
+
+        if (courseCode != null && !courseCode.isEmpty()) {
+            exprAttr.put(":code", new AttributeValue().withS(courseCode));
+            ScanRequest scanReq = new ScanRequest()
+                    .withTableName("t_csd_course")
+                    .withFilterExpression("contains(course_code,:code)")
+                    .withProjectionExpression("course_name,course_time,instructor")
+                    .withExpressionAttributeValues(exprAttr);
+            ScanResult result = client.scan(scanReq);
+            Map<String, String> courseInfo = retrieveCourseTime(result);
+            timeConcat = courseInfo.get("time");
+            courseName = courseInfo.get("name");
+            courseName = courseName;
+
+            speechText = speechText = "Course CS" + courseCode + " " + courseName + " starts " + timeConcat;
+            input.getAttributesManager().setSessionAttributes(Collections.singletonMap("course_name", courseName));
+            if(!request.getDialogState().toString().equals("COMPLETED")){
+                Slot nameSlot = Slot.builder().withName("course_name").withValue(courseName).build();
+                Slot codeSlot = Slot.builder().withName("course_code").withValue(courseCode).build();
+                Intent returnedIntent = request.getIntent().builder().putSlotsItem("course_name",nameSlot).putSlotsItem("course_code",codeSlot).withName("TimeTableIntent").build();
+                return input.getResponseBuilder().addDelegateDirective(returnedIntent).build();
+            }
+        } else if (courseName != null && !courseName.equals("")) {
+            exprAttr.put(":name", new AttributeValue().withS(courseName));
+            ScanRequest scanReq = new ScanRequest()
+                    .withTableName("t_csd_course")
+                    .withFilterExpression("course_name = :name")
+                    .withProjectionExpression("course_code, course_time")
+                    .withExpressionAttributeValues(exprAttr);
+            ScanResult result = client.scan(scanReq);
+            Map<String, String> courseInfo = retrieveCourseTime(result);
+            if (courseInfo.containsKey("code")) {
+                codeConcat = courseInfo.get("code");
+            }
+            if (courseInfo.containsKey("time")) {
                 timeConcat = courseInfo.get("time");
-                courseName = courseInfo.get("name");
-                if (timeConcat.startsWith("Sorry")) {
-                    speechText = timeConcat;
-                } else {
-                    speechText = "CS" + courseCode + " " + courseName + " starts " + timeConcat;
-                }
-            } else if (courseName!=null && !courseName.equals("")) {
-                exprAttr.put(":name", new AttributeValue().withS(courseName));
-                ScanRequest scanReq = new ScanRequest()
-                        .withTableName("t_csd_timetable")
-                        .withFilterExpression("course_name = :name")
-                        .withProjectionExpression("course_code, course_time")
-                        .withExpressionAttributeValues(exprAttr);
-                ScanResult result = client.scan(scanReq);
-                Map<String,String> courseInfo = retrieveCourseTime(result);
-                if(courseInfo.containsKey("code")){
-                    codeConcat = courseInfo.get("code");
-                }
-                if(courseInfo.containsKey("time")){
-                    timeConcat = courseInfo.get("time");
-                }
-                if (timeConcat.startsWith("Sorry")) {
-                    speechText = timeConcat;
-                } else {
-                    speechText = "Course CS" + codeConcat + " " + courseName + " starts " + timeConcat;
-                }
             }
-        }
-        catch(Exception ex){
-            speechText = "error occured. "+ ex.getLocalizedMessage();
-        }
-
-        //if(!courseCode.equals("")){
-        //    key.put("course_code", AttributeValue.builder().s(courseCode).build());
-        //}
-        /**
-         DynamoDbClient dbClient = DynamoDbClient.create();
-         Map<String, AttributeValue> key = new HashMap<String, AttributeValue>();
-         if(!courseName.equals("")){
-         key.put("course_name", AttributeValue.builder().s(courseName).build());
-         }
-        GetItemRequest itemRequest = GetItemRequest.builder().key(key).tableName("t_csd_timetable").build();
-        try {
-            Map<String, AttributeValue> returned = dbClient.getItem(itemRequest).item();
-            Course course;
-            String name = "";
-            List<String> code = new ArrayList<String>();
-            String location = "";
-            List<String> time = new ArrayList<String>();
-            String codeConcat = "";
-            String timeConcat = "";
-            speechText = "I fetched data from dynamoDB.";
-            for(String attrName:returned.keySet()){
-                switch(attrName){
-                    case "course_name":
-                        name = returned.get(attrName).s();
-                        break;
-                    case "course_code":
-                        List<AttributeValue> codes = returned.get(attrName).l();
-                        for(AttributeValue codeAttr:codes){
-                            code.add(codeAttr.s());
-                            codeConcat = codeConcat+ codeAttr.s()+ " or ";
-                        }
-                        codeConcat =codeConcat.substring(0,codeConcat.lastIndexOf(" or "));
-                        break;
-                    case "course_location":
-                        location = returned.get(attrName).s();
-                        break;
-                    case "course_time":
-                        List<AttributeValue> times = returned.get(attrName).l();
-                        for(AttributeValue timeAttr:times){
-                            time.add(timeAttr.s());
-                            timeConcat = timeConcat + timeAttr.s() + " or ";
-                        }
-                        timeConcat = timeConcat.substring(0, timeConcat.length()-3);
-                        break;
-
-                }
+            if (timeConcat.startsWith("Sorry")) {
+                speechText = timeConcat;
+            } else {
+                speechText = "Course CS" + codeConcat + " " + courseName + " starts " + timeConcat;
             }
-            //Course.builder().courseName(name).courseCode(code).courseTimes(time).courseLocation(location).build();
+            return input.getResponseBuilder().
+                    withSimpleCard("CSD Assistant",speechText).withSpeech(speechText).build();
+        }
 
-            speechText = "CS"+ codeConcat + " " +name+ " starts "+timeConcat;
-        }
-        catch(DynamoDbException dde){
-            System.err.println(dde.getMessage());
-            speechText = "Error occurred. "+dde.getMessage();
-        }
-         **/
         return input.getResponseBuilder().
                 withSimpleCard("CSD Assistant",speechText).withSpeech(speechText).build();
 
