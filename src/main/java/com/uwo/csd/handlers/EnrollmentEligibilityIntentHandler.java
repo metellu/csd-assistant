bddb3b88f568e9394d8e69aa3ec7f84ca209f191
metellu <metellu@hotmail.com>
2019-07-04 22:42:53 -0400

    made changes to code in order to enable unlimited round of prerequisites check.

--- src/main/java/com/uwo/csd/handlers/EnrollmentEligibilityIntentHandler.java
+++ src/main/java/com/uwo/csd/handlers/EnrollmentEligibilityIntentHandler.java
@@ -57,7 +57,7 @@ public class EnrollmentEligibilityIntentHandler implements IntentRequestHandler 
                 courseName = (String)attr.get("course_name");
             }
             else{
-                return input.getResponseBuilder().addElicitSlotDirective("course_name",intentRequest.getIntent()).withSpeech("Which course you would like to check?").build();
+                return input.getResponseBuilder().addElicitSlotDirective("course_name",intentRequest.getIntent()).withSpeech("Please tell me which course you would like to check?").build();
             }
         }
 
@@ -107,21 +107,27 @@ public class EnrollmentEligibilityIntentHandler implements IntentRequestHandler 
                 Map<String, AttributeValue> item = items.get(0);
                 if (item.containsKey("prerequisites")) {
                     List<AttributeValue> attrVals = item.get("prerequisites").getL();
-                    for (AttributeValue attrVal : attrVals) {
-                        prerequisite.add(attrVal.getS());
-                        preqConcat += attrVal.getS() + ", ";
+                    if( attrVals.size()== 0 ){
+                        speechText = "Sorry, it seems that there is no available prerequisite for that course.";
                     }
-                    courseName = (courseName!=null && !courseName.isEmpty())?courseName:item.get("course_name").getS();
-                    Map<String,Object> sessionAttr = input.getAttributesManager().getSessionAttributes();
-                    sessionAttr.put("course_name",courseName);
-                    input.getAttributesManager().setSessionAttributes(sessionAttr);
-                    //preqConcat = preqConcat.substring(0, preqConcat.lastIndexOf(","));
-                    //preqConcat = preqConcat.substring(0,preqConcat.lastIndexOf(","))+" and " + preqConcat.substring(preqConcat.lastIndexOf(",")+2);
-                    speechText = "According to the course description of " +courseName + ", students are expected to meet all the prerequisites to enroll in the course. Please provide yes or no answers to the following questions. Then I can help you determine your eligibility. " + prerequisite.get(0);
-                    prerequisite.remove(0);
-                    attr.put("prerequisites",prerequisite);
-                    input.getAttributesManager().setSessionAttributes(attr);
-                    return input.getResponseBuilder().addElicitSlotDirective("prerequisites_confirm", intentRequest.getIntent()).withSpeech(speechText).build();
+                    else {
+                        for (AttributeValue attrVal : attrVals) {
+                            prerequisite.add(attrVal.getS());
+                            preqConcat += attrVal.getS() + ", ";
+                        }
+                        courseName = (courseName != null && !courseName.isEmpty()) ? courseName : item.get("course_name").getS();
+                        Map<String, Object> sessionAttr = input.getAttributesManager().getSessionAttributes();
+                        sessionAttr.put("course_name", courseName);
+                        input.getAttributesManager().setSessionAttributes(sessionAttr);
+                        //preqConcat = preqConcat.substring(0, preqConcat.lastIndexOf(","));
+                        //preqConcat = preqConcat.substring(0,preqConcat.lastIndexOf(","))+" and " + preqConcat.substring(preqConcat.lastIndexOf(",")+2);
+                        speechText = "According to the course description of " + courseName + ", students are expected to meet all the prerequisites to enroll in the course. Please provide yes or no answers to the following questions. Then I can help you determine your eligibility. " + prerequisite.get(0);
+                        prerequisite.remove(0);
+                        attr.put("prerequisites", prerequisite);
+                        input.getAttributesManager().setSessionAttributes(attr);
+                        return input.getResponseBuilder().addElicitSlotDirective("prerequisites_confirm", intentRequest.getIntent()).withSpeech(speechText).build();
+                    }
+
                 }
             }
             catch(Exception ex){
