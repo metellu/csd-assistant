701565f92c874c9d8fb754de9176884762bcf3dd
metellu <metellu@hotmail.com>
2019-07-04 22:53:22 -0400

    re-adjust the properties of the course entity

--- src/main/java/com/uwo/csd/entity/Course.java
+++ src/main/java/com/uwo/csd/entity/Course.java
@@ -3,13 +3,16 @@ package com.uwo.csd.entity;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 @Data
 @Builder
-public class Course {
+public class Course implements Serializable {
     private String courseName;
     private List<String> courseCode;
-    private String courseLocation;
-    private List<String> courseTimes;
+    private List<Map<String,String>> courseTimeLoc;
+    private List<String> instructors;
+    private String description;
 }
