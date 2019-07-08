1036f806d4db429df7fd393976dbd15a3d2db59c
metellu <metellu@hotmail.com>
2019-07-08 16:25:22 -0400

    enable the intent to prompt user with choices to query the instructors' contact info if the user fails the eligibility checkup.

--- src/main/java/com/uwo/csd/handlers/EnrollmentEligibilityIntentHandler.java
+++ src/main/java/com/uwo/csd/handlers/EnrollmentEligibilityIntentHandler.java
@@ -29,16 +29,50 @@ public class EnrollmentEligibilityIntentHandler implements IntentRequestHandler 
         String courseCode = "";
         String preqConcat = "";
         String confirm    = "";
+        String degree     = "";
+        String instructor_confirm = "";
         List<String> prerequisite = new ArrayList<>();
 
         Map<String,Slot> slots = intentRequest.getIntent().getSlots();
         courseName = IntentHelper.getCourseNameIfExists(slots);
         courseCode = IntentHelper.getCourseCodeIfExists(slots);
+        degree     = IntentHelper.getSpecifiedSlotValueIfExists(slots,"degree");
+
+        if( !IntentHelper.isStringValid(degree) ){
+            return input.getResponseBuilder().addElicitSlotDirective("degree",intentRequest.getIntent()).withSpeech("In order to proceed, I would like to know if you are a graduate student or undergraduate student.").build();
+        }
+        else{
+            degree = slots.get("degree").getResolutions().getResolutionsPerAuthority().get(0).getValues().get(0).getValue().getName();
+        }
         try {
             confirm = slots.get("prerequisites_confirm").getResolutions().getResolutionsPerAuthority().get(0).getValues().get(0).getValue().getName();
         }
         catch(Exception ex){
             speechText = ex.getMessage();
+        }
+        try{
+            instructor_confirm = slots.get("instructor_info_confirm").getResolutions().getResolutionsPerAuthority().get(0).getValues().get(0).getValue().getName();
+        }
+        catch(Exception ex){
+
+        }
+
+        if( IntentHelper.isStringValid(instructor_confirm) && instructor_confirm.equalsIgnoreCase("yes") ){
+            Map<String,Object> sessionAttr = input.getAttributesManager().getSessionAttributes();
+            if( sessionAttr.containsKey("instructor") ){
+                String instructors = (String)sessionAttr.get("instructor");
+                String[] inst = instructors.split("\\|");
+                speechText = "You can reach ";
+                for(String instr:inst) {
+                    Map<String, AttributeValue> exprAttr = new HashMap<>();
+                    exprAttr.put(":name",new AttributeValue().withS(instr));
+                    List<Map<String,AttributeValue>> items = IntentHelper.DBQueryByInstructorName(exprAttr,"fullname,email");
+                    if( items.size()==1 ){
+                        speechText += "Professor "+IntentHelper.capitalizeName(items.get(0).get("fullname").getS())+" at "+items.get(0).get("email").getS()+" ";
+                    }
+                }
+            }
+            return input.getResponseBuilder().withSpeech(speechText).withSimpleCard("CSD Assistant",speechText).withShouldEndSession(false).build();
         }
 
         List<String> confirms = new ArrayList<>();
@@ -66,9 +100,13 @@ public class EnrollmentEligibilityIntentHandler implements IntentRequestHandler 
             attr.put("confirms",confirms);
             if (confirms.stream().anyMatch(str -> IntentHelper.isStringValid(str))) {
                 if (confirms.stream().anyMatch(str -> str.equalsIgnoreCase("no"))) {
-                    speechText = "Sorry, it appears you don't meet all the prerequisites. You cannot enroll in this course.";
+                    speechText = "Unfortunately, it appears you don't meet all the prerequisites. You probably will not be allowed to enroll. However, you still can communicate with the instructor to gain his or her approval for enrollment. Would you like to know his or her contact info?";
                     confirms.clear();
                     attr.replace("confirms",confirms);
+                    prerequisite.clear();
+                    attr.replace("prerequisites",prerequisite);
+                    input.getAttributesManager().setSessionAttributes(attr);
+                    return input.getResponseBuilder().addElicitSlotDirective("instructor_info_confirm",intentRequest.getIntent()).withSpeech(speechText).build();
                 }
                 else {
 
@@ -78,22 +116,22 @@ public class EnrollmentEligibilityIntentHandler implements IntentRequestHandler 
                         input.getAttributesManager().setSessionAttributes(attr);
                         return input.getResponseBuilder().addElicitSlotDirective("prerequisites_confirm", intentRequest.getIntent()).withSpeech(cond).build();
                     } else {
-                        speechText = "Congrats. You meet all the prerequisites. You may be able to enroll in this course.";
+                        speechText = "Congratulations. You meet all the prerequisites. You may be able to enroll in this course. ";
                     }
                 }
             }
         }
         else {
-            AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+            //AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
             List<Map<String, AttributeValue>> items = new ArrayList<>();
             try {
                 if ( IntentHelper.isStringValid(courseName) ) {
                     Map<String, AttributeValue> exprAttr = new HashMap<String, AttributeValue>();
                     exprAttr.put(":courseName", new AttributeValue().withS(courseName));
-                    items = IntentHelper.DBQueryByCourseName(exprAttr,"prerequisites,course_name,course_code");
+                    items = IntentHelper.DBQueryByCourseName(exprAttr,"prerequisites,course_name,course_code,instructor");
 
                     if (items.size() == 0) {
-                        return input.getResponseBuilder().addElicitSlotDirective("course_name", intentRequest.getIntent()).withSpeech("Sorry, the course you're looking for seems unavailable this year. Would you like to try another course?").build();
+                        return input.getResponseBuilder().addElicitSlotDirective("course_name", intentRequest.getIntent()).withSpeech("Sorry, the course you're looking for seems unavailable this year. Please provide me with a course name, if you would like to try another course.").build();
                     }
                 }
                 else if( IntentHelper.isStringValid(courseCode) ){
@@ -101,12 +139,38 @@ public class EnrollmentEligibilityIntentHandler implements IntentRequestHandler 
                     exprAttr.put(":courseCode", new AttributeValue().withS(courseCode));
                     items = IntentHelper.DBQueryByCourseCode(exprAttr,"prerequisites,course_name,course_code");
                     if (items.size() == 0) {
-                        return input.getResponseBuilder().addElicitSlotDirective("course_name", intentRequest.getIntent()).withSpeech("Sorry, the course you're looking for seems unavailable this year. Would you like to try another course?").build();
+                        return input.getResponseBuilder().addElicitSlotDirective("course_name", intentRequest.getIntent()).withSpeech("Sorry, the course you're looking for seems unavailable this year. Please provide me with a course name, if you would like to try another course.").build();
                     }
                 }
                 Map<String, AttributeValue> item = items.get(0);
                 if (item.containsKey("prerequisites")) {
-                    List<AttributeValue> attrVals = item.get("prerequisites").getL();
+                    Map<String,AttributeValue> attrVals = item.get("prerequisites").getM();
+                    if( attrVals.containsKey(degree) ){
+                        List<AttributeValue> attrList = attrVals.get(degree).getL();
+                        for( AttributeValue attrVal: attrList ){
+                            prerequisite.add(attrVal.getS());
+                        }
+                        speechText = "According to the course description of " + courseName + ", students are expected to meet all the prerequisites to enroll in the course. Please provide yes or no answers to the following questions. Then I can help you determine your eligibility. " + prerequisite.get(0);
+                        prerequisite.remove(0);
+                        String instructor = IntentHelper.formInstructorString(item.get("instructor").getL());
+                        attr.put("prerequisites", prerequisite);
+                        attr.put("instructor",instructor);
+                        input.getAttributesManager().setSessionAttributes(attr);
+                        return input.getResponseBuilder().addElicitSlotDirective("prerequisites_confirm", intentRequest.getIntent()).withSpeech(speechText).build();
+                    }
+                    else{
+                        speechText = "It appears that there is no prequisites list for a "+degree+" student. You may be able to enroll in that course.";
+                    }
+                    courseName = (courseName != null && !courseName.isEmpty()) ? courseName : item.get("course_name").getS();
+                    Map<String, Object> sessionAttr = input.getAttributesManager().getSessionAttributes();
+                    sessionAttr.put("course_name", courseName);
+                    input.getAttributesManager().setSessionAttributes(sessionAttr);
+                    //preqConcat = preqConcat.substring(0, preqConcat.lastIndexOf(","));
+                    //preqConcat = preqConcat.substring(0,preqConcat.lastIndexOf(","))+" and " + preqConcat.substring(preqConcat.lastIndexOf(",")+2);
+
+
+                }
+                /**
                     if( attrVals.size()== 0 ){
                         speechText = "Sorry, it seems that there is no available prerequisite for that course.";
                     }
@@ -127,8 +191,9 @@ public class EnrollmentEligibilityIntentHandler implements IntentRequestHandler 
                         input.getAttributesManager().setSessionAttributes(attr);
                         return input.getResponseBuilder().addElicitSlotDirective("prerequisites_confirm", intentRequest.getIntent()).withSpeech(speechText).build();
                     }
+                 }
+                 **/
 
-                }
             }
             catch(Exception ex){
                 speechText = ex.getMessage();
