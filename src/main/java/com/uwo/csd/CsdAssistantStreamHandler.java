44d413c4a0ce59f8fa634437bc111c2d319025bd
metellu <metellu@hotmail.com>
2019-07-09 01:03:29 -0400

    register the new intent handler

--- src/main/java/com/uwo/csd/CsdAssistantStreamHandler.java
+++ src/main/java/com/uwo/csd/CsdAssistantStreamHandler.java
@@ -16,6 +16,7 @@ public class CsdAssistantStreamHandler extends SkillStreamHandler {
                         new InstructorIntentHandler(),
                         new EnrollmentEligibilityIntentHandler(),
                         new CoursesWithinSpecifiedTermIntentHandler(),
+                        new CoursesOnSpecifiedDayIntentHandler(),
                         new VenueIntentHandler(),
                         new EventIntentHandler(),
                         new HelpIntentHandler(),
