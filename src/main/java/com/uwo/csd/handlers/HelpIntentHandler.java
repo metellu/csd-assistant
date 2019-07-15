3e771cc1d5c8af105be94f9794d3824ff59f6607
metellu <metellu@hotmail.com>
2019-07-15 15:36:13 -0400

    rewrite the help message to include samples for invoking all functions

--- src/main/java/com/uwo/csd/handlers/HelpIntentHandler.java
+++ src/main/java/com/uwo/csd/handlers/HelpIntentHandler.java
@@ -17,10 +17,10 @@ public class HelpIntentHandler implements RequestHandler {
 
     @Override
     public Optional<Response> handle(HandlerInput input) {
-        String speechText = "You can ask questions about course time, description, instructor info as well as your eligibility to enroll into a certain course. For instant, you may ask 'Am I allowed to enroll in Intro to data science'";
+        String speechText = "You can ask questions about course description, course time, venue, evaluation method, instructor info as well as your eligibility to enroll into a certain course. For instant, you may say 'tell me something about unstructured data' to check course description, 'check the time for unstructured data' to check the course time and venue, 'tell me something about professor Hanan Lutfiyya' to get a brief description of the instructor, 'show me the evaluation method of intro to data science' to get a brief narration of the course evaluation, or 'Am I allowed to enroll in Intro to data science' to check your eligibility of course enrollment";
         return input.getResponseBuilder()
                 .withSpeech(speechText)
-                .withSimpleCard("HelloWorld", speechText)
+                .withSimpleCard("CSD Assistant", speechText)
                 .withReprompt(speechText)
                 .build();
     }
